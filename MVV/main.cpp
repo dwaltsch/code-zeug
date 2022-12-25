4a4bed8e64c6a4266ec240c6fb569f89475afdd3
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-12-25 18:34:02 +0100

    flawed logic

--- MVV/main.cpp
+++ MVV/main.cpp
@@ -13,38 +13,12 @@ int getmagicnumber(int n){
 }
 
 char solve(string s) {
-    bool ismagic = false;
-    int size = s.size();
+    bool jump = false;
     int magicnumber = getmagicnumber(s.length());
-    int jumpnumber = getmagicnumber(s.size()-magicnumber);
-    while (s.size() > magicnumber || ismagic == false) {
+    int sndjump = getmagicnumber(s.length() - magicnumber);
+    while (s.size() > magicnumber) {
         string newString;
-        if((size - jumpnumber) == s.size()){
-            int jumpsize = jumpnumber - magicnumber;
-            string schring;
-            schring += s[0];
-            schring += s[jumpsize];
-            schring += s[s.size() - 1 - jumpsize];
-            schring += s[s.size()-1];
-            string b = schring;
-            for (int i = 0; i < b.size() - 1; i++) {
-                if (b[i] == b[i + 1]) {
-                    newString += b[i];
-                } else {
-                    if (b[i] != 'R' && b[i + 1] != 'R') {
-                        newString += 'R';
-                    } else if (s[i] != 'G' && s[i + 1] != 'G') {
-                        newString += 'G';
-                    } else {
-                        newString += 'B';
-                    }
-                }
-
-            }
-            s = newString;
-            ismagic = true;
-        }
-        else{
+        if(s.length() > magicnumber + sndjump){
             for (int i = 0; i < s.size() - 1; i++) {
                 if (s[i] == s[i + 1]) {
                     newString += s[i];
@@ -59,9 +33,30 @@ char solve(string s) {
                 }
             }
         }
+        else{
+            string solvestring;
+            solvestring += s[0];
+            solvestring += s[sndjump - 1];
+            solvestring += s[s.size() - 1 - sndjump];
+            solvestring += s[s.size() - 1];
+            for (int i = 0; i < solvestring.size() - 1; i++) {
+                if (solvestring[i] == solvestring[i + 1]) {
+                    newString += s[i];
+                } else {
+                    if (solvestring[i] != 'R' && solvestring[i + 1] != 'R') {
+                        newString += 'R';
+                    } else if (solvestring[i] != 'G' && solvestring[i + 1] != 'G') {
+                        newString += 'G';
+                    } else {
+                        newString += 'B';
+                    }
+                }
+            }
+            jump = true;
+        }
         s = newString;
     }
-    if(s.size() == magicnumber || ismagic == true){
+    if(jump == 1 || s.length() == magicnumber){
         string firstLast;
         firstLast += s[0];
         firstLast += s[s.size()-1];
@@ -81,6 +76,6 @@ char solve(string s) {
 }
 
 int main() {
-    cout << solve("RRRRRRRRRRRRRRRRRRRRRRRRRR") << endl;
+    cout << solve("RGBRGBRGBRRGBRGB") << endl;
     return 0;
 }
